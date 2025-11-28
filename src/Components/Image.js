f117ea2a5ca2670156aeae34c6b4c53253f5cda8
Shubhjeet <shubhjeet.calphawizz@gmail.com>
2025-11-28 14:55:16 +0530

    home page

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -8,6 +8,18 @@ import DantistDoctor from "../images/dantist-doctor-img.jpg";
 import AboutUsImg from "../images/about-us-img-1.jpg";
 import AboutUsImg2 from "../images/about-us-img-2.jpg";
 import AboutExpreanceImg from "../images/about-experience-circle.png";
+import IconServices1 from "../images/icon-services-1.svg";
+import IconServices2 from "../images/icon-services-2.svg";
+import IconServices3 from "../images/icon-services-3.svg";
+import IconServices4 from "../images/icon-services-4.svg";
+import whyChooseImg1 from "../images/icon-why-us-1.svg";
+import whyChooseImg2 from "../images/icon-why-us-2.svg";
+import whyChooseImg3 from "../images/icon-why-us-3.svg";
+import whyChooseImg4 from "../images/icon-why-us-4.svg";
+import whyChooseImg5 from "../images/icon-why-us-5.svg";
+import whyChooseImg6 from "../images/icon-why-us-6.svg";
+import whyChooseImg from "../images/why-choose-us-img.png";
+
 export {
   Logo,
   HeroImg,
@@ -19,4 +31,15 @@ export {
   AboutUsImg,
   AboutUsImg2,
   AboutExpreanceImg,
+  IconServices1,
+  IconServices2,
+  IconServices3,
+  IconServices4,
+  whyChooseImg,
+  whyChooseImg2,
+  whyChooseImg3,
+  whyChooseImg1,
+  whyChooseImg4,
+  whyChooseImg5,
+  whyChooseImg6,
 };
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,6 +9,17 @@ import {
   AboutUsImg,
   AboutUsImg2,
   AboutExpreanceImg,
+  IconServices3,
+  IconServices1,
+  IconServices2,
+  IconServices4,
+  whyChooseImg,
+  whyChooseImg2,
+  whyChooseImg3,
+  whyChooseImg1,
+  whyChooseImg4,
+  whyChooseImg5,
+  whyChooseImg6,
 } from "../Components/Image";
 
 const Home = () => {
@@ -105,24 +116,24 @@ const Home = () => {
         </div>
       </div>
 
-      <div class="about-us">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-lg-6">
-              <div class="about-image">
-                <div class="about-img-1">
-                  <figure class="image-anime">
+      <div className="about-us">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="about-image">
+                <div className="about-img-1">
+                  <figure className="image-anime">
                     <img src={AboutUsImg} alt="" />
                   </figure>
                 </div>
 
-                <div class="about-img-2">
-                  <figure class="image-anime">
+                <div className="about-img-2">
+                  <figure className="image-anime">
                     <img src={AboutUsImg2} alt="" />
                   </figure>
                 </div>
 
-                <div class="about-experience">
+                <div className="about-experience">
                   <figure>
                     <img src={AboutExpreanceImg} alt="" />
                   </figure>
@@ -130,21 +141,24 @@ const Home = () => {
               </div>
             </div>
 
-            <div class="col-lg-6">
-              <div class="about-content">
-                <div class="section-title">
-                  <h3 class="wow fadeInUp">about us</h3>
-                  <h2 class="text-anime-style-2" data-cursor="-opaque">
+            <div className="col-lg-6">
+              <div className="about-content">
+                <div className="section-title">
+                  <h3 className="wow fadeInUp">about us</h3>
+                  <h2 className="text-anime-style-2" data-cursor="-opaque">
                     <span>Your Journey</span> to a Healthier Smile Begins Here
                   </h2>
-                  <p class="wow fadeInUp" data-wow-delay="0.25s">
+                  <p className="wow fadeInUp" data-wow-delay="0.25s">
                     The goal of our clinic is to provide friendly, caring
                     dentistry and the highest level of general, cosmetic, and
                     specialist dental treatments. With dental practices
                     throughout the world.
                   </p>
                 </div>
-                <div class="about-us-body wow fadeInUp" data-wow-delay="0.5s">
+                <div
+                  className="about-us-body wow fadeInUp"
+                  data-wow-delay="0.5s"
+                >
                   <ul>
                     <li>experienced team</li>
                     <li>comprehensive services</li>
@@ -153,16 +167,354 @@ const Home = () => {
                   </ul>
                 </div>
                 <div
-                  class="about-us-footer wow fadeInUp"
+                  className="about-us-footer wow fadeInUp"
                   data-wow-delay="0.75s"
                 >
-                  <a href="#" class="btn-default">
+                  <a href="#" className="btn-default">
                     read more about us
                   </a>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="our-services">
+        <div className="container">
+          <div className="row section-row">
+            <div className="col-lg-12">
+              <div className="section-title">
+                <h3 className="wow fadeInUp">our services</h3>
+                <h2 className="text-anime-style-2" data-cursor="-opaque">
+                  <span>Hight Quality</span> Services for You.
+                </h2>
+                <p className="wow fadeInUp" data-wow-delay="0.25s">
+                  We are committed to sustainability. eco-friendly initiatives.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-6">
+              <div className="service-item wow fadeInUp">
+                <div className="icon-box">
+                  <div className="img">
+                    <img src={IconServices1} alt="" />
+                  </div>
+                </div>
+                <div className="service-body">
+                  <h3>general dental care</h3>
+                  <p>
+                    We are excited to meet you and provide the best dental care
+                    for your family.
+                  </p>
+                </div>
+                <div className="read-more-btn">
+                  <a href="#">read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6">
+              <div className="service-item wow fadeInUp" data-wow-delay="0.25s">
+                <div className="icon-box">
+                  <div className="img">
+                    <img src={IconServices2} alt="" />
+                  </div>
+                </div>
+                <div className="service-body">
+                  <h3>dental implants</h3>
+                  <p>
+                    We are excited to meet you and provide the best dental care
+                    for your family.
+                  </p>
+                </div>
+                <div className="read-more-btn">
+                  <a href="#">read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6">
+              <div className="service-item wow fadeInUp" data-wow-delay="0.5s">
+                <div className="icon-box">
+                  <div className="img">
+                    <img src={IconServices3} alt="" />
+                  </div>
+                </div>
+                <div className="service-body">
+                  <h3>cosmetic dentistry</h3>
+                  <p>
+                    We are excited to meet you and provide the best dental care
+                    for your family.
+                  </p>
+                </div>
+                <div className="read-more-btn">
+                  <a href="#">read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6">
+              <div className="service-item wow fadeInUp" data-wow-delay="0.75s">
+                <div className="icon-box">
+                  <div className="img">
+                    <img src={IconServices4} alt="" />
+                  </div>
+                </div>
+                <div className="service-body">
+                  <h3>teeth whitening</h3>
+                  <p>
+                    We are excited to meet you and provide the best dental care
+                    for your family.
+                  </p>
+                </div>
+                <div className="read-more-btn">
+                  <a href="#">read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-12">
+              <div
+                className="services-box-footer wow fadeInUp"
+                data-wow-delay="1s"
+              >
+                <p>
+                  We believe in using the latest technology and techniques to
+                  ensure the best outcomes for our patients.
+                </p>
+                <a href="#" className="btn-default">
+                  view all service
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="intro-clinic-video">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="visit-clinic parallaxie">
+                  <div className="visit-clinic-content">
+                    <div className="section-title">
+                      <h3 className="wow fadeInUp">visit clinic</h3>
+                      <h2 className="text-anime-style-2" data-cursor="-opaque">
+                        Comprehensive Dental Care For All Ages
+                      </h2>
+                    </div>
+                    <div
+                      className="visit-clinic-btn wow fadeInUp"
+                      data-wow-delay="0.25s"
+                      data-cursor-text="Play"
+                    >
+                      <a href="#" className="popup-video play-btn">
+                        play video
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="our-counter">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="counter-item">
+                  <div className="counter-title">
+                    <h2>
+                      <span className="counter">75</span>+
+                    </h2>
+                    <h3>insurance covered</h3>
+                  </div>
+                  <div className="counter-content">
+                    <p>
+                      Our team loves dental trivia. Did you know that tooth
+                      enamel.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6">
+                <div className="counter-item">
+                  <div className="counter-title">
+                    <h2>
+                      <span className="counter">02</span>K
+                    </h2>
+                    <h3>realized projects</h3>
+                  </div>
+                  <div className="counter-content">
+                    <p>
+                      Our team loves dental trivia. Did you know that tooth
+                      enamel.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6">
+                <div className="counter-item">
+                  <div className="counter-title">
+                    <h2>
+                      <span className="counter">22</span>K
+                    </h2>
+                    <h3>happy customers</h3>
+                  </div>
+                  <div className="counter-content">
+                    <p>
+                      Our team loves dental trivia. Did you know that tooth
+                      enamel.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6">
+                <div className="counter-item">
+                  <div className="counter-title">
+                    <h2>
+                      <span className="counter">18</span>+
+                    </h2>
+                    <h3>experience doctors</h3>
+                  </div>
+                  <div className="counter-content">
+                    <p>
+                      Our team loves dental trivia. Did you know that tooth
+                      enamel.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="icon-star-image">
+          <img src={StarIcon} alt="" />
+        </div>
+      </div>
+
+      <div class="why-choose-us">
+        <div class="container">
+          <div class="row section-row">
+            <div class="col-lg-12">
+              <div class="section-title">
+                <h3 class="wow fadeInUp">why choose us</h3>
+                <h2 class="text-anime-style-2" data-cursor="-opaque">
+                  <span>Diagnosis of</span> Dental Diseases
+                </h2>
+                <p class="wow fadeInUp" data-wow-delay="0.25s">
+                  We are committed to sustainability. eco-friendly initiatives.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-4 col-md-6 order-1">
+              <div class="why-choose-box-1">
+                <div class="why-choose-item wow fadeInUp">
+                  <div class="icon-box">
+                    <img src={whyChooseImg1} alt="" />
+                  </div>
+                  <div class="why-choose-content">
+                    <h3>experienced doctor</h3>
+                    <p>
+                      The goal of our clinic is to provide friendly, caring
+                      dentistry and the.
+                    </p>
+                  </div>
+                </div>
+                <div
+                  class="why-choose-item wow fadeInUp"
+                  data-wow-delay="0.25s"
+                >
+                  <div class="icon-box">
+                    <img src={whyChooseImg2} alt="" />
+                  </div>
+                  <div class="why-choose-content">
+                    <h3>personalized care</h3>
+                    <p>
+                      The goal of our clinic is to provide friendly, caring
+                      dentistry and the.
+                    </p>
+                  </div>
+                </div>
+                <div class="why-choose-item wow fadeInUp" data-wow-delay="0.5s">
+                  <div class="icon-box">
+                    <img src={whyChooseImg3} alt="" />
+                  </div>
+                  <div class="why-choose-content">
+                    <h3>flexible payment options</h3>
+                    <p>
+                      The goal of our clinic is to provide friendly, caring
+                      dentistry and the.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 order-lg-1 order-md-2 order-1">
+              <div class="why-choose-image wow fadeInUp">
+                <figure>
+                  <img src={whyChooseImg} alt="" />
+                </figure>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 order-lg-2 order-md-1 order-2">
+              <div class="why-choose-box-2">
+                <div class="why-choose-item wow fadeInUp">
+                  <div class="icon-box">
+                    <img src={whyChooseImg4} alt="" />
+                  </div>
+                  <div class="why-choose-content">
+                    <h3>emergency services</h3>
+                    <p>
+                      The goal of our clinic is to provide friendly, caring
+                      dentistry and the.
+                    </p>
+                  </div>
+                </div>
+                <div
+                  class="why-choose-item wow fadeInUp"
+                  data-wow-delay="0.25s"
+                >
+                  <div class="icon-box">
+                    <img src={whyChooseImg5} alt="" />
+                  </div>
+                  <div class="why-choose-content">
+                    <h3>positive patient reviews</h3>
+                    <p>
+                      The goal of our clinic is to provide friendly, caring
+                      dentistry and the.
+                    </p>
+                  </div>
+                </div>
+                <div class="why-choose-item wow fadeInUp" data-wow-delay="0.5s">
+                  <div class="icon-box">
+                    <img src={whyChooseImg6} alt="" />
+                  </div>
+                  <div class="why-choose-content">
+                    <h3>latest technology</h3>
+                    <p>
+                      The goal of our clinic is to provide friendly, caring
+                      dentistry and the.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="icon-star-image">
+          <img src={StarIcon} alt="" />
         </div>
       </div>
     </>
